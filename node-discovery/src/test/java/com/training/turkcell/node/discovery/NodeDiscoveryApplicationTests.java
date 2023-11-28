a4bdb50b618d31cdcad744ee3ddfad930b6bd0fc
sozgat <said.ozgat@hotmail.com>
2023-11-28 10:56:30 +0300

    +eureka server added

---
+++ node-discovery/src/test/java/com/training/turkcell/node/discovery/NodeDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.turkcell.node.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodeDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
