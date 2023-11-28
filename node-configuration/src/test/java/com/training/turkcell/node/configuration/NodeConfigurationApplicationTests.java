26291a7c32402662ea6a76b34fcc6755e29230b5
osman yaycioglu <osman.yaycioglu@gmail.com>
2023-11-28 09:36:18 +0300

    Initial commit

---
+++ node-configuration/src/test/java/com/training/turkcell/node/configuration/NodeConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.turkcell.node.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodeConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
